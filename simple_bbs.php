c14fb716692d1497d7687ee7592e8b8c0a17f549
nununugit <hiraganadenu@gmail.com>
2019-11-06 10:12:01 +0900

    add: コードの整形

--- simple_bbs.php
+++ simple_bbs.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
-        <title>掲示板（簡易版）</title>
-    </head>
-        <body>
-        <form action="simple_bbs.php" method="post">
-        <p>
-        名前:<br>
-        <input type="text" name="users_names" size="40">
-        </p><p>
-        コメント: <br>
-        <textarea name="users_comments" rows="4" cols="50"></textarea>
-        </p><p>
-        <input type="submit" name="投稿" >
-        </p><p>
-        <input type="reset" value="リセット">
-        </p>        
-        </form>
-        <form action="simple_bbs.php" method="post">
-        <p>
-        <input type="submit" name="全削除" >
-        </p>     
-        </form>
-        <?php
+<?php
         //MySQLサーバへの接続とデータベースの選択
         $dsn='mysql:dbname=jikken1;host=localhost;charset=utf8';
         $user='root';
@@ -53,25 +28,55 @@
             if(!$result){
                 die($dbh ->error);
             }
-        }
-        if(isset($_GET['id'])){
+        }if(isset($_GET['id'])){
                     $id =  @$_GET['id'];
                     $sql = "UPDATE simple_bbs SET delete_flag = 1 WHERE id=$id;";
                     $result = $dbh ->query($sql);
                     if(!$result){
                         die($dbh ->error);
                     }
-                }
+        }if(isset($_POST['delete_all'])){
+                    $sql = "INSERT INTO simple_bbs VALUES( '','sucess', 'sucess',0 );";
+                    $result = $dbh ->query($sql);
+                    if(!$result){
+                        die($dbh ->error);
+                    }
+        }
         ?>
-
-    <table border="1">
+    
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+        <title>掲示板（簡易版）</title>
+    </head>
+        <body>
+        <form action="simple_bbs.php" method="post">
+        <p>
+        名前:<br>
+        <input type="text" name="users_names" size="40">
+        </p><p>
+        コメント: <br>
+        <textarea name="users_comments" rows="4" cols="50"></textarea>
+        </p><p>
+        <input type="submit" name="投稿" >
+        </p><p>
+        <input type="reset" value="リセット">
+        </p>        
+        </form>
+        <form action="simple_bbs.php" method="get">
+        <p>
+        <input type="submit" value="全削除" name="delete_all"" >
+        </p>     
+        </form>
+        <table border="1">
         <tr><th>name</th><th>comment</th><th>delete_button</th></tr>
         <?php foreach($data as $row){ ?>
             <tr>
             <td><?php echo $row['users_names'];?></td>
             <td><?php echo $row['users_comments'];?></td>
             <td>
-            <form action="simple_bbs.php" method="get">
+            <form action="simple_bbs.php" method="post">
         <input type="submit" value="削除する" >
         <input type="hidden" name="id" value="<?=$row['id']?>">
         </form>
@@ -81,3 +86,5 @@
         </table>        
         </body>
     </html>
+
+        
