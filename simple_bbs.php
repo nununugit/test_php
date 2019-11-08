ec6bdb651239ae40d76f64e846c037267fc08535
nununugit <hiraganadenu@gmail.com>
2019-11-08 22:31:00 +0900

    add: xss対策の追加

--- simple_bbs.php
+++ simple_bbs.php
@@ -17,7 +17,7 @@
             print ($e->getMessage());
             die();
         }
-        //テーブルへの登録
+            //テーブルへの登録
         if(isset($_POST['users_names'])){
             $name = @$_POST['users_names'];
             $comment = @$_POST['users_comments'];
@@ -35,6 +35,7 @@
             }
             header('Location: ./simple_bbs.php');}
            
+            //個別削除
         }if(isset($_GET['id'])){
                     $id =  @$_GET['id'];
                     $sql = "UPDATE simple_bbs SET delete_flag = 1 WHERE id=$id;";
@@ -44,7 +45,9 @@
                     }
                     header('Location: ./simple_bbs.php');
 
+            //全削除
         }if(isset($_POST['delete_all'])){
+            date_default_timezone_set('Asia/Tokyo');
             $timestamp = time() ;
             $now= date( "Y/m/d H:i:s", $timestamp ) ;
             $sql="UPDATE simple_bbs SET delete_flag = 1 WHERE delete_flag = 0;
@@ -62,46 +65,49 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>掲示板（簡易版）</title>
+        <title>ひとこと掲示板</title>
     </head>
         <body>
         <form action="simple_bbs.php" method="post">
-        <p>
+        <div class="form-group"><p>
         名前:<br>
-        <input type="text" name="users_names" size="40">
-        </p><p>
-        コメント: <br>
-        <textarea name="users_comments" rows="4" cols="50"></textarea>
-        </p><p>
-        <input type="submit" name="投稿" >
-        </p><p>
-        <input type="reset" value="リセット">
-        </p>        
-        </form>
-        <form action="simple_bbs.php" method="post">
-        <p>
-        <input type="submit" value="全削除" name="delete_all" >
+        <input type="text" placeholder="name" name="users_names" size="40">
         </p>
+        
+        <p>
+        コメント: <br>
+        <textarea placeholder="comment" name="users_comments" rows="4" cols="50"></textarea>
+        </p>
+
+        <input class="btn btn-primary mb-2" type="submit" name="投稿" >
+        <input class="btn btn-primary mb-2" type="reset" value="リセット">        
         </form>
+        
+        <form action="simple_bbs.php" method="post">
+        <input class="btn btn-primary mb-2" type="submit" value="全削除" name="delete_all" >
+        </div>
+        </form>
+        
         <div class="table-responsive">
-        <table class="table">
+            <table class="table">
         <thead class="thead-dark">
         <tr><th>投稿者</th><th>コメント</th><th>コメント削除</th><th>投稿日</th></tr>
         <?php foreach($data as $row){ ?>
             <tr>
-            <td><?php echo $row['users_names'];?></td>
-            <td><?php echo $row['users_comments'];?></td>
+            <td><?php echo htmlentities( $row['users_names'], ENT_QUOTES, 'UTF-8');?></td>
+            <td><?php echo htmlentities( $row['users_comments'], ENT_QUOTES, 'UTF-8');;?></td>
             <td>
             <form action="simple_bbs.php" method="get">
-        <input type="submit" value="削除する" >
-        <input type="hidden" name="id" value="<?=$row['id']?>">
-        <td><?php echo $row['post_date'];?></td>
+            <input type="submit" value="削除する" >
+            <input type="hidden" name="id" value="<?=$row['id']?>">
+            </td>
         </form>
-    </td>
+        <td><?php echo $row['post_date'];?></td>
         </tr>
-        <?php }?>
-        </table>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <?php }?>        
+            </table>
+        </div>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
  <!-- Bootstrap Javascript(jQuery含む) -->
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
