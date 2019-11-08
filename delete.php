1105afcfc41053a274f36f0a2561d1d7c71fd1db
nununugit <hiraganadenu@gmail.com>
2019-11-08 20:52:10 +0900

    add: 投稿後のリダイレクトの追加と見目の整形

--- delete.php
+++ delete.php
@@ -6,7 +6,6 @@ $password= '';
 try{
     $dbh =new PDO($dsn,$user,$password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT * FROM simple_bbs";
     $stmt=$dbh->prepare($sql);
     $stmt->execute();
     $count = $stmt->rowCount();
@@ -17,15 +16,10 @@ try{
     print ($e->getMessage());
     die();
 }
-
-if(isset($_GET['id'])){
-            $id =  @$_GET['id'];
-            $name = @$_GET['users_names'];
-            $comment = @$_GET['users_comments'];
-            $sql = "UPDATE simple_bbs SET delete_flag = 1 WHERE id=$id;";
-            $result = $dbh ->query($sql);
-            if(!$result){
-                die($dbh ->error);
-            }
-        }
-        ?>
+?>
+<!--bootstrap入れようね
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+-->
--- simple_bbs.php
+++ simple_bbs.php
@@ -13,27 +13,28 @@
             while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                 $data[]=$row;
             }
-            echo '処理が完了しました';
         }catch(PDOException $e){
             print ($e->getMessage());
             die();
         }
-
         //テーブルへの登録
         if(isset($_POST['users_names'])){
             $name = @$_POST['users_names'];
             $comment = @$_POST['users_comments'];
             if (empty($name)||empty($comment)){
-                echo "名前かコメントを入力してください。";
+                echo "<br>";
+                echo '<div class="alert alert-primary" role="alert"><strong>文字を入力してください</strong></div>';
             }else{
             date_default_timezone_set('Asia/Tokyo');
             $timestamp = time() ;
-            $now= date( "Y/m/d H:i:s", $timestamp ) ;
+            $now= date( "Y/m/d H:i:s", $timestamp );
             $sql = "INSERT INTO simple_bbs VALUES( '','$name', '$comment',0,'$now' );";
             $result = $dbh ->query($sql);
             if(!$result){
                 die($dbh ->error);
-            }}
+            }
+            header('Location: ./simple_bbs.php');}
+           
         }if(isset($_GET['id'])){
                     $id =  @$_GET['id'];
                     $sql = "UPDATE simple_bbs SET delete_flag = 1 WHERE id=$id;";
@@ -41,46 +42,26 @@
                     if(!$result){
                         die($dbh ->error);
                     }
+                    header('Location: ./simple_bbs.php');
+
         }if(isset($_POST['delete_all'])){
             $timestamp = time() ;
             $now= date( "Y/m/d H:i:s", $timestamp ) ;
-            $sql = "INSERT INTO simple_bbs VALUES( '','$name', '$comment',0,'$now' );";
-                    $sql = "DROP TABLE `simple_bbs`;
-                    CREATE TABLE `simple_bbs` (
-                      `id` int(11) NOT NULL,
-                      `users_names` varchar(11) NOT NULL,
-                      `users_comments` text NOT NULL,
-                      `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
-                      `post_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-                    
-                    ALTER TABLE `simple_bbs`
-                      ADD PRIMARY KEY (`id`);
-                    
-                    --
-                    -- ダンプしたテーブルのAUTO_INCREMENT
-                    --
-                    
-                    --
-                    -- テーブルのAUTO_INCREMENT `simple_bbs`
-                    --
-                    ALTER TABLE `simple_bbs`
-                      MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
-                    COMMIT;
-                      INSERT INTO simple_bbs VALUES( '','master', 'first comment',0,'$now' );
-                      ";
+            $sql="UPDATE simple_bbs SET delete_flag = 1 WHERE delete_flag = 0;
+            INSERT INTO simple_bbs VALUES( '','master', 'firstcomment',0,'$now' );";
                     $result = $dbh ->query($sql);
                     echo "success";
                     if(!$result){
                         die($dbh ->error);
                     }
-        }
-        ?>
+                    header('Location: ./simple_bbs.php');
+        }?>
     
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>掲示板（簡易版）</title>
     </head>
         <body>
@@ -99,10 +80,12 @@
         </form>
         <form action="simple_bbs.php" method="post">
         <p>
-        <input type="submit" value="全削除" name="delete_all"" >
-        </p>     
+        <input type="submit" value="全削除" name="delete_all" >
+        </p>
         </form>
-        <table>
+        <div class="table-responsive">
+        <table class="table">
+        <thead class="thead-dark">
         <tr><th>投稿者</th><th>コメント</th><th>コメント削除</th><th>投稿日</th></tr>
         <?php foreach($data as $row){ ?>
             <tr>
@@ -117,6 +100,12 @@
     </td>
         </tr>
         <?php }?>
-        </table>        
+        </table>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ <!-- Bootstrap Javascript(jQuery含む) -->
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        
         </body>
     </html>
